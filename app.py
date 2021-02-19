1b0444d33f4239d369cc75740d8cd86b354e5716
Janhvi Joshi <500060253@stu.upes.ac.in>
2021-02-19 12:27:30 +0530

    Fixed minor bug

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.eviron.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'  # this key needs to more complicated and secure
 api = Api(app)  # makes it easier for us to define what CRUD operations can be defined for each resource of our api
